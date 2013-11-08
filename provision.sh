f3d3baef9b8a484bea250f3cde988bc1c95d7b48
laierr <kenderberator@gmail.com>
2013-11-08 22:11:00 +0200

    Take two

--- provision.sh
+++ provision.sh
@@ -3,6 +3,30 @@
 echo "HELL YEAH"
 
 apt-get install -y nginx
-service nginx restart
 
-echo "W00T" > /usr/share/nginx/html/index.html
+mkdir /opt/w00t
+
+echo "W00T" > /opt/w00t/index.html
+
+cat > /etc/nginx/sites-enabled/W00T <<CONF
+server {
+        listen 80;
+        listen [::]:80 default_server ipv6only=on;
+
+        root /opt/w00t;
+        index index.html;
+
+        server_name localhost;
+
+		location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ /index.html;
+                # Uncomment to enable naxsi on this location
+                # include /etc/nginx/naxsi.rules
+        }
+    }
+CONF
+
+unlink /etc/nginx/sites-enabled/default
+service nginx restart
